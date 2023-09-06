33657bd2ba21eebc67c5c171e5824804583dba45
Enes TOPRAK <etoprakpc@gmail.com>
2023-09-06 10:18:22 +0300

    .

--- FunctionApp2/Function1.cs
+++ FunctionApp2/Function1.cs
@@ -1,39 +1,33 @@
 using System;
-using Microsoft.Azure.Functions.Worker;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
-namespace FunctionApp2
+namespace FunctionApp1
 {
     public class Function1
     {
-        private readonly ILogger _logger;
-
-        public Function1(ILoggerFactory loggerFactory)
+        [FunctionName("Function1")]
+        public static void Run([TimerTrigger("1 * * * * *")] TimerInfo myTimer, ILogger log)
         {
-            _logger = loggerFactory.CreateLogger<Function1>();
+
+
         }
-
-        [Function("Function1")]
-        public void Run([TimerTrigger("0 */5 * * * *")] MyInfo myTimer)
-        {
-            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
-        }
-    }
-
-    public class MyInfo
-    {
-        public MyScheduleStatus ScheduleStatus { get; set; }
-
-        public bool IsPastDue { get; set; }
-    }
-    //gfgfgf
-    public class MyScheduleStatus
-    {
-        public DateTime Last { get; set; }
-
-        public DateTime Next { get; set; }
-
-        public DateTime LastUpdated { get; set; }
     }
 }
+
+/*
+                    
+                                                                         %%%%%%
+                                                                       %%%%%%
+                                                             @   %%%%%%    @
+                                                      @@   %%%%%%      @@
+                                            @@@    %%%%E%%%%%%    @@@
+                                          @@      %%%%%T%%%%        @@
+                                                @@       %%%%%%       @@
+                                                       @@      %%%       @@
+                                                         @@    %%      @@
+                                                                  %%
+                                                                 %
+
+*/
